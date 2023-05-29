0cfc81ce2d66dbb29c6e826a00b9b32b22c1120c
HarshZode <zodeharsh@gmail.com>
2023-05-29 18:50:39 +0530

    updated database link

--- backend/database/mongoose.js
+++ backend/database/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require ('mongoose')
 
 mongoose.Promise = global.Promise
 
-mongoose.connect('mongodb://127.0.0.1:27017/taskmanager', {useNewUrlParser: true, useUnifiedTopology:true})
+mongoose.connect(process.env.MONGO_LINK, {useNewUrlParser: true, useUnifiedTopology:true})
     .then(() => console.log('Database Connected'))
     .catch((error) => console.log(error))
 
